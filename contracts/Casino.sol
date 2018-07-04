d685807945baa85ad967d6f6d732f8ddbdc18f4f
LeoTheG <leonargharib@gmail.com>
2018-07-03 20:47:39 -0700

    Init for ethereum decentralized app

---
+++ contracts/Casino.sol
@@ -0,0 +1,13 @@
+pragma solidity 0.4.20;
+
+contract Casino {
+    address public owner;
+
+    function Casino() public {
+        owner = msg.sender;
+    }
+
+    function kill() public {
+        if(msg.sender == owner) selfdestruct(owner);
+    }
+}
